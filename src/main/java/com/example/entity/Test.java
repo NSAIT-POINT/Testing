f6b8b076ab852379c63952256074792a5d24b15f
NSA IT-POINT <nsaitpoints@gmail.com>
2023-01-29 13:37:58 +0530

    added

--- src/main/java/com/example/entity/Test.java
+++ src/main/java/com/example/entity/Test.java
@@ -10,6 +10,7 @@ public class Test {
 	public static void main(String[] args) {
 
 	}
+<<<<<<< Updated upstream
 void m1(){
 	// logic
 }
@@ -18,4 +19,18 @@ void m2() {
 	
 	// if i complteted m2 method 
 }
+=======
+
+	void m1() {
+		if(10==10) {
+			System.out.println("10 isequal to 10");
+		}else {
+			
+		}
+		
+	}
+	
+	
+	
+>>>>>>> Stashed changes
 }
