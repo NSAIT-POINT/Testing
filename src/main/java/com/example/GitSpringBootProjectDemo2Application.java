7e7d5222d722593e0a989993dbd4fb88267e9098
NSA IT-POINT <nsaitpoints@gmail.com>
2023-01-29 13:04:11 +0530

    adding

---
+++ src/main/java/com/example/GitSpringBootProjectDemo2Application.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitSpringBootProjectDemo2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitSpringBootProjectDemo2Application.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/entity/Test.java
@@ -0,0 +1,10 @@
+package com.example.entity;
+
+public class Test {
+public static void main(String[] args) {
+	
+	
+	
+}
+
+}
